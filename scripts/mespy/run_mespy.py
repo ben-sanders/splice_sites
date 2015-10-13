58e1e9c7d8dd054167868a0fc8373947c1f8d051
Ben Sanders <ben@bensanders.co.uk>
2015-10-13 16:55:45 +0100

    run_mespy now has score_site function that returns rank and score changes

--- scripts/mespy/run_mespy.py
+++ scripts/mespy/run_mespy.py
@@ -12,24 +12,30 @@ Score site position by change in ranking, e.g. drop one place = -1, stay the sam
 import mespy
 from operator import itemgetter
 
-def score_ranking(wt_site, wt_scores, var_scores):
+def score_site(wt_site, wt_scores, var_scores):
     """ 
     given the wt site (which may just be the highest scoring), locate in the variant list, and
     return the position difference between the two lists.
+
+    Inputs:
+        wt_site    - site (ideally true wild-type splice site, being tested)
+        wt_scores  - list of scores from wild-type sequence
+        var_scores - list of scores from variant sequence 
+    Outputs:    
+        (rank change, percentage change)
     """
-    print wt_site
-    print "---------------------"
+    
+    # find the rank (position) of the test site in wt and variant score lists
     wt_rank = find_site(wt_site, wt_scores)
     var_rank = find_site(wt_site, var_scores)
-    print "rank score:", wt_rank-var_rank
-
+    
+    # use rank to find the exact score (rank is 0 indexed, as are the score lists)
     wt_score = wt_site[3]
     var_score = var_scores[var_rank][3]
-    print wt_score, var_score
     # take wt from var, so increase == positive, decrease == negative
-    score_change = var_score - wt_score
-    print score_change
-    print var_score / wt_score * 100
+    score_change = var_score / wt_score * 100
+    rank_change = wt_rank - var_rank
+    return rank_change, score_change
 
 def find_site(test_site, score_list):
     """
@@ -37,13 +43,18 @@ def find_site(test_site, score_list):
     Need to search by position, not the whole record - won't be found in var list!
     If not found, for whatever reason (there shouldn't be a length restriction to the list), returns -1.
     """
+    # get position of the test splice site, this will allow comparison to the same point
     test_site_position = test_site[1]
     position = 0
+    
+    # count through the list until you find the position you're looking for
+    # then return the index it is at.
     for site in [x[1] for x in score_list]:
         if site == test_site_position:
             return position
         else:
             position += 1
+    # if not found, return -1
     return -1
 
 def generate_scores(seq1, seq2):
@@ -58,11 +69,15 @@ def generate_scores(seq1, seq2):
     
     return scores, scores2
 
-test = "CCGCATTTGTACTGTGTTAAGCAATAAAAAGTATACCTTTTTGTTGTCAGGCTTGAGAAAGAGATCCAAGATCTTGAAAAAGCTGAACTGCAAATCTCAA"
-test2 = "ACTACGAGCATCGACGGCGACGACTATCTACTACTACTAGCAGCTACATTATATAGCAGCACTATCAGCTCTAGCTGCGATGCTAGCTACGATCGTATAT"
+def mespy_test():
+    """
+    Just a wrapper for testing purposes.
+    """
+    test = "CCGCATTTGTACTGTGTTAAGCAATAAAAAGTATACCTTTTTGTTGTCAGGCTTGAGAAAGAGATCCAAGATCTTGAAAAAGCTGAACTGCAAATCTCAA"
+    test2 = "ACTACGAGCATCGACGGCGACGACTATCTACTACTACTAGCAGCTACATTATATAGCAGCACTATCAGCTCTAGCTGCGATGCTAGCTACGATCGTATAT"
 
-wt_scores, var_scores = generate_scores(test, test2)
-score_ranking(wt_scores[0], wt_scores, var_scores)
+    wt_scores, var_scores = generate_scores(test, test2)
+    rank_change, score_change = score_site(wt_scores[0], wt_scores, var_scores)
 
-
-# check the differences by position
+if __name__ == "__main__":
+    mespy_test()
